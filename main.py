bf49eff93017d1ae1d1e4f327f88aff11aca8d6a
JMateus1997 <joaomateus_22@msn.com>
2026-01-30 21:29:20 -0300

    Adição dos restantes gráficos para a conclusão da atividade

--- main.py
+++ main.py
@@ -25,6 +25,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from pyparsing import alphas
 
 df = pd.read_csv('ecommerce_preparados.csv')
 
@@ -33,17 +34,53 @@ print(df.tail().to_string())
 
 # Gráfico de Histograma - Cód da Marca com o Preço
 sns.jointplot(x='Marca_Cod', y='Preço', data=df, kind='hist')
+plt.title('Relação Marca e Preço')
+plt.xlabel('Marca')
+plt.ylabel('Preço')
 plt.show()
 
 # Gráfico de Dispersão
 # As variáveis que serão avaliadas serão Material e Temporada, para saber se existe correlação do tipo do Material com as condições climáticas
 
 sns.jointplot(x='Material_Cod', y='Temporada_Cod', data=df, kind='scatter')
+plt.title('Dispersão Material e Temporada')
+plt.xlabel('Material')
+plt.ylabel('Temporada')
 plt.show()
 
 # Mapa de Calor - Desconto com a Temporada_Cod e Marca_Cod
 corr = df[['Marca_Cod', 'Temporada_Cod', 'Desconto']].corr()
+plt.title('Mapa de Calor- Desconto e Temporada')
+plt.xlabel('Marca')
+plt.ylabel('Temporada')
 sns.heatmap(corr, annot=True, cmap='Blues')
 plt.show()
 
+# Gráfico de barra = Quais marcas são mais compradas
+df['Qtd_Vendidos_Cod'].value_counts().plot(kind='bar', color='red')
+plt.title('Quantidade Vendidas')
+plt.xlabel('Qtd_Vendidas')
+plt.xticks(rotation=45)
+plt.ylabel('Contagem de Qtd_Vendidas')
+plt.show()
+
+# Gráfico de Pizza
+plt.figure(figsize=(10, 10))
+plt.pie(df['Qtd_Vendidos_Cod'].value_counts(), autopct='%.1f%%')
+plt.title('Distribuição de Quantidade Vendidas')
+plt.show()
+
+# Gráfico de Densidade
+plt.figure(figsize=(10, 7))
+sns.kdeplot(df['Marca_Cod'], fill=True, color='green')
+plt.title('Densidade de Marcas')
+plt.xlabel('Marca')
+plt.show()
+
+# Gráfico de Regressão
+sns.regplot(x='Marca_Cod', y='Preço', data=df, color='blue', scatter_kws={'alpha': 0.5, 'color': '#34c289'})
+plt.title('Regressão entre Marcas e Preços')
+plt.xlabel('Marca')
+plt.ylabel('Preço')
+plt.show()
 
